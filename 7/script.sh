53a45e626ed861a6f4921e54fe11dbbb1d3353b9
Roman Shevchuk <rommssh@gmail.com>
2016-05-09 23:41:04 +0300

    Problem #7 added

--- 7/script.sh
+++ 7/script.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+ 
+user=root
+ 
+while [ "$1" != "" ];
+do
+	server=$1
+    memory=(`ssh ${user}@${server} free | grep Mem`)
+ 
+    freemem=${memory[3]}
+    if [ ${freemem} -lt  1024000 ]
+    then
+        echo $server " | " $freemem
+    fi
 
+    shift
+done
