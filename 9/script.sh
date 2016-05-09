af3f18eb7ddcbff68a091d78c4a0a897c441136b
Roman Shevchuk <rommssh@gmail.com>
2016-05-10 00:17:11 +0300

    Problem #9 added

--- 9/script.sh
+++ 9/script.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+wget http://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/latest-stage3.txt
+
+lastd=/`egrep -ro [0-9]+/stage3-amd64-[0-9]+.tar.bz2 latest-stage3.txt`
+paket=`egrep -ro stage3-amd64-[0-9]+.tar.bz2 latest-stage3.txt`
+
+echo $lastd
+
+wget http://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds${lastd}
+tar xf $paket -C /media
