914bc75659e32155c22f0d8da5d75a42785e08a3
Roman Shevchuk <rommssh@gmail.com>
2016-05-10 00:03:39 +0300

    Problem #8 added

--- 8/script.sh
+++ 8/script.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+domain=$1
+dnsa=$(host -t A "${domain}" | awk '{print $4}' | tr "\n" " ")
+
+for ip in ${dnsa[*]}
+do
+	ping -c2 $ip > /dev/null && echo $ip is available || echo $ip is NOT available
+done
